72b5bee2e658855b769c70c637ea09954ec2a6d0
Nils Schmidt <nsdn@gmx.de>
2011-12-18 22:20:13 +0100

    added coding dojo kata #6

---
+++ src/main/scala/dojo/kata6/ProjectEuler23.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2011 by Nils Schmidt.
+ */
+package dojo.kata6
+
+import scala.util._
+
+/**
+ * @author Nils
+ */
+object ProjectEuler23 {
+  private val LIMIT = 28123 // 10000 // 1000
+
+  /**
+   * Creates all abundant numbers between 12 and 28123. (Sorted!)
+   * @see Project Euler 23 for clarification of the limits.
+   */
+  def allAbundants = {
+    List[Int]()
+  }
+
+  /**
+   * Sum of proper divisors.
+   */
+  def sumOfProperDivisors(number: Int): Long = {
+    0
+  }
+
+  /**
+   * Numbers that cannot be expressed as the sum of any abundant numbers.
+   */
+  def notExpressableAsSumOfAbundantNumbers: List[Int] = {
+    List[Int]()
+  }
+}
