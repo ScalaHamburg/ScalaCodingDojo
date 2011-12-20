135f6e89755c5f6e75bc0944c0600a2c8e325929
Nils Schmidt <nils.schmidt@logica.com>
2011-12-20 20:01:59 +0100

    Added ProjectEuler10 as kata10

---
+++ src/main/scala/dojo/kata10/ProjectEuler10.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2011 by Nils Schmidt.
+ */
+package dojo.kata10
+import scala.annotation.tailrec
+
+/**
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * Find the sum of all the primes below two million.
+ * (142913828922)
+ *
+ * @author Nils Schmidt
+ */
+object ProjectEuler10 {
+  def sumOfPrimes(n: Long): Long = {0}
+}
