db390cba37fcebfd84ff005283fb59de989c44de
PTR <e@mail.com>
2012-01-09 17:49:18 +0100

    My Try

--- src/main/scala/dojo/kata6/ProjectEuler23.scala
+++ src/main/scala/dojo/kata6/ProjectEuler23.scala
@@ -16,20 +16,33 @@ object ProjectEuler23 {
    * @see Project Euler 23 for clarification of the limits.
    */
   def allAbundants = {
-    List[Int]()
+    (12 to LIMIT).filter(n=>sumOfProperDivisors(n)>n)
   }
 
   /**
    * Sum of proper divisors.
    */
   def sumOfProperDivisors(number: Int): Long = {
-    0
+    divisors(number).sum
   }
 
   /**
    * Numbers that cannot be expressed as the sum of any abundant numbers.
    */
   def notExpressableAsSumOfAbundantNumbers: List[Int] = {
-    List[Int]()
+    val abundants = allAbundants
+    (1 to LIMIT).filter(n=>isNotSumOfAnyTwoInOrderedList(n, abundants)).toList
+  }
+  
+  def isNotSumOfAnyTwoInOrderedList(n:Int, l:IndexedSeq[Int]) = {
+    val list = l.takeWhile(a=>a<n).reverse
+//    println(list)
+    val isNotSum = ! list.exists(x=>list.exists(y=>x+y==n))
+    if(isNotSum){println(n+" ist nicht summe abundanter Zahlen")}
+    isNotSum
+  }
+  
+  def divisors(a: Int):List[Int] = {
+    (2 to a/2).filter(d=>(a%d)==0).toList
   }
 }
