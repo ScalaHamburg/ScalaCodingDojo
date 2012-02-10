568e13229c1939a3937497b418411abaa14d9aa2
PTR <e@mail.com>
2012-02-10 21:59:29 +0100

    Implementierung von https://github.com/shadaj/euler/blob/master/src/euler/Euler2.scala siehe auch http://www.youtube.com/watch?v=eWMqR0x_aDg

---
+++ src/main/scala/dojo/kata4/FibonacciShadaj.scala
@@ -0,0 +1,18 @@
+package dojo.kata4
+
+/**
+ * Leicht an unsere Bedürfnisse Angepasste Implementierung von:
+ * https://github.com/shadaj/euler/blob/master/src/euler/Euler2.scala
+ */
+object FibonacciShadaj {
+
+  def nextFib(x: Int, y: Int): Stream[Int] = {
+    x #:: nextFib(y, x+y)
+  }
+
+  def fib = nextFib(1,1)
+  
+  def solve(max: Int) = {
+    fib.takeWhile(_ <= 4000000).filter(_ % 2 == 0).sum
+  }
+}
